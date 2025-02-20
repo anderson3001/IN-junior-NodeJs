6f63e952f30d4af19cd86544f864d1eef9652f27
Anderson Gomes <derson3001@gmail.com>
2025-02-20 18:25:29 -0300

    feat: repositorio de usuarios

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,22 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "../../http/lib/prisma";
+import { UsersRepository } from "../users-repostory";
 
-export class PrismaUsersRepository {
+export class PrismaUsersRepository implements UsersRepository {
+    async findByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+        return user
+    }
+
     async create(data: Prisma.UserCreateInput) {
         const user = await prisma.user.create({
             data
         })
 
-        return { user }
+        return user 
     }
 }
--- src/use-cases/register-use-cases.ts
+++ src/use-cases/register-use-cases.ts
@@ -1,5 +1,5 @@
 import { hash } from "bcryptjs"
-import { prisma } from "../http/lib/prisma"
+import { UsersRepository } from "../repositories/users-repostory"
 
 interface RegisterUseCaseRequest {
     name: string
@@ -9,14 +9,10 @@ interface RegisterUseCaseRequest {
 
 export class RegisterUseCase{
 
-    constructor(private usersRepository: any) {}
+    constructor(private usersRepository: UsersRepository) {}
 
     async execute({name, email, password}: RegisterUseCaseRequest){
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
     
         if (userWithSameEmail) {
             throw new Error()
