9f6d8596550f5a69951603c56ae86576ef942ed3
Anderson Gomes <derson3001@gmail.com>
2025-02-20 17:31:05 -0300

    feat: routes e organização de código

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,8 @@
 import { PrismaClient } from "@prisma/client"
 import fastify from "fastify"
 import { z } from "zod"
+import { appRoutes } from "./http/routes"
 
 export const app = fastify()
-const prisma = new PrismaClient()
 
-app.post('/', async (request, reply) => {//criando usuario
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
-
-    const { name, email, password } = registerBodySchema.parse(request.body)
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password
-        }
-    })
-
-    return reply.status(201).send("Usuário criado com sucesso!")
-})
+app.register(appRoutes)
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+import { z } from "zod"
+import { prisma } from "../lib/prisma"
+
+export async function register(request: FastifyRequest,reply: FastifyReply) {//criando usuario
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        }
+    })
+
+    return reply.status(201).send("Usuário criado com sucesso!")
+}
