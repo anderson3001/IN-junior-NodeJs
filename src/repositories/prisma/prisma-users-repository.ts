d685dde6107bf41387dec44ad765b50d0599dd54
Anderson Gomes <derson3001@gmail.com>
2025-02-21 16:18:17 -0300

    feat: patch do user

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,23 @@
 import { Prisma, User} from "@prisma/client";
 import { prisma } from "../../http/lib/prisma";
-import { UsersRepository } from "../users-repostory";
+import { UsersRepository, UserUpdateInput } from "../users-repostory";
 
 export class PrismaUsersRepository implements UsersRepository {
+    async update(id: string, data: UserUpdateInput): Promise<User | null> {
+        const user = await prisma.user.update({
+            where: {
+                id
+            },
+            data: {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+                photo: data.photo
+            }
+        })
+        return user
+    
+    }
     async delete(id: string): Promise<User | null> {
         const posts = await prisma.post.deleteMany({//pra deletar os posts antes de deletar o usuario
             where: {
