f302410a481fb39d315e7bde5b9df2a1d0c732df
Anderson Gomes <derson3001@gmail.com>
2025-02-20 21:01:30 -0300

    feat: get no user

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,16 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 import { prisma } from "../../http/lib/prisma";
 import { UsersRepository } from "../users-repostory";
 
 export class PrismaUsersRepository implements UsersRepository {
+    async findById(id: string){
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+        return user 
+    }
     async findByEmail(email: string) {
         const user = await prisma.user.findUnique({
             where: {
